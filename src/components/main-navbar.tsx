1b4e88312eda7a3b019ae6e97e6b5ebb11c972b4
cecvl <christiancecil.dev@gmail.com>
2025-07-07 17:11:41 +0300

    fixed navbar

--- src/components/main-navbar.tsx
+++ src/components/main-navbar.tsx
@@ -10,59 +10,73 @@ import { Link } from "react-router-dom";
 
 export function MainNav() {
   return (
-    <NavigationMenu className="px-4 py-3 border-b shadow-sm sticky top-0 bg-white z-50">
-      <NavigationMenuList className="flex gap-4">
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link to="/" className="hover:underline">
-              Home
-            </Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
+    <div className="sticky top-0 z-50 w-full bg-white border-b shadow-sm">
+      <div className="relative px-6 py-4 max-w-7xl mx-auto">
+        <NavigationMenu>
+          <NavigationMenuList className="flex gap-6 items-center">
+            {/* Home */}
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link to="/" className="text-sm font-medium hover:underline">
+                  Home
+                </Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Account</NavigationMenuTrigger>
-          <NavigationMenuContent className="p-4 w-32">
-            <ul className="space-y-2">
-              <li>
-                <NavigationMenuLink asChild>
-                  <Link to="/signin" className="block hover:underline">
-                    Sign In
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
+            {/* Account Dropdown */}
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="text-sm font-medium">
+                Account
+              </NavigationMenuTrigger>
+              <NavigationMenuContent className="p-4 w-40">
+                <ul className="space-y-2">
+                  <li>
+                    <NavigationMenuLink asChild>
+                      <Link
+                        to="/signin"
+                        className="block text-sm hover:underline"
+                      >
+                        Sign In
+                      </Link>
+                    </NavigationMenuLink>
+                  </li>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Join ArtPrints Kanairo</NavigationMenuTrigger>
-          <NavigationMenuContent className="p-4 w-48">
-            <ul className="space-y-2">
-              <li>
-                <NavigationMenuLink asChild>
-                  <Link
-                    to="/signup?type=artist"
-                    className="block hover:underline"
-                  >
-                    Sign up as Artist
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-              <li>
-                <NavigationMenuLink asChild>
-                  <Link
-                    to="/signup?type=printshop"
-                    className="block hover:underline"
-                  >
-                    Sign up as Print Shop
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+            {/* Join Dropdown */}
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="text-sm font-medium">
+                Join ArtPrints Kanairo
+              </NavigationMenuTrigger>
+              <NavigationMenuContent className="p-4 w-52">
+                <ul className="space-y-2">
+                  <li>
+                    <NavigationMenuLink asChild>
+                      <Link
+                        to="/signup?type=artist"
+                        className="block text-sm hover:underline"
+                      >
+                        Sign up as Artist
+                      </Link>
+                    </NavigationMenuLink>
+                  </li>
+                  <li>
+                    <NavigationMenuLink asChild>
+                      <Link
+                        to="/signup?type=printshop"
+                        className="block text-sm hover:underline"
+                      >
+                        Sign up as Print Shop
+                      </Link>
+                    </NavigationMenuLink>
+                  </li>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
   );
 }
